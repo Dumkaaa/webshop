f8c97f16f1ddd8c05be2f26296cb84725d9e3f29
Dmitriy Larionov <dumkaaa72@gmail.com>
2020-10-23 13:13:23 +0200

    JS and SCSS support for checkbox tables added.

--- assets/admin/js/_tables.js
+++ assets/admin/js/_tables.js
@@ -2,14 +2,74 @@
     /**
      * Make a whole table row work as a link.
      */
-    document.getElementsByClassName('table-row-link').forEach(function(row) {
+    document.getElementsByClassName('table-row-link').forEach(function (row) {
         row.addEventListener('click', function (event) {
-            if (event.target.href) {
-                // The user clicked on a link within the table row, ignore.
+            if (event.target instanceof HTMLAnchorElement  || event.target instanceof HTMLInputElement || event.target.classList.contains('no-click')) {
+                // The user clicked on an anchor field, input field or field with the no-click class within the table row, ignore.
                 return;
             }
 
             window.location.href = row.dataset.href;
         });
+    });
+
+    /**
+     * Handle table checkboxes.
+     */
+    document.getElementsByClassName('table-checkbox').forEach(function (table) {
+        const actionRow = table.getElementsByClassName('checkbox-action-row')[0];
+        const actionAnchors = actionRow.getElementsByTagName('a');
+        const countSpan = actionRow.getElementsByClassName('count')[0];
+        const headerCheckboxes = table.querySelectorAll('thead input[type="checkbox"]');
+        const headerCheckbox = headerCheckboxes.length > 0 ? headerCheckboxes[0] : null;
+        const bodyCheckboxes = table.querySelectorAll('tbody input[type="checkbox"]');
+
+        // Handle thead checkbox clicks.
+        if (headerCheckbox) {
+            headerCheckbox.addEventListener('click', function () {
+                bodyCheckboxes.forEach(function (checkbox) {
+                    checkbox.checked = headerCheckbox.checked;
+                });
+
+                updateTableActions();
+            });
+        }
+
+        // Handle tbody checkbox clicks.
+        bodyCheckboxes.forEach(function (checkbox) {
+            checkbox.addEventListener('click', updateTableActions);
+        });
+
+        // Update the actions row.
+        function updateTableActions() {
+            // Index all values of the selected checkboxes.
+            let selectedValues = [];
+            bodyCheckboxes.forEach(function (checkbox) {
+                if (checkbox.checked) {
+                    selectedValues.push(checkbox.dataset.value);
+                }
+            });
+
+            // Update the header checkbox state (unchecked, checked, indeterminate).
+            if (headerCheckbox) {
+                headerCheckbox.checked = selectedValues.length === bodyCheckboxes.length;
+                headerCheckbox.indeterminate = selectedValues.length > 0 && selectedValues.length !== bodyCheckboxes.length;
+            }
+
+            // Update the action row visibility and count.
+            actionRow.classList.toggle('show', selectedValues.length > 0);
+            countSpan.innerHTML = selectedValues.length;
+
+            actionAnchors.forEach(function (anchor) {
+                let href = anchor.dataset.baseHref;
+                if (!href) {
+                    // This is not an action with a base href, ignore.
+                    return;
+                }
+
+                href += '?values=' + selectedValues.join(',');
+                anchor.href = href;
+            });
+        }
     });
 })();
